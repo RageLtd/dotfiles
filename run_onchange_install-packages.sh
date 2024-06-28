3d3244970f2d3a3dbc6d3129eeede70ca4def93c
RageLtd <rageltd@pm.me>
2024-06-27 21:18:58 -0700

    Add onchange script

---
+++ run_onchange_install-packages.sh
@@ -0,0 +1,28 @@
+#! /bin/sh
+set -eo pipefail
+
+# If we're on arch, and don't already have it, install paru
+
+if [ -f /etc/arch-release ] && ! command -v paru &> /dev/null; then
+    sudo pacman -S --needed base-devel
+    git clone https://aur.archlinux.org/paru.git
+    cd paru
+    makepkg -si
+    cd ..
+    rm -rf paru
+fi
+
+# Figure out which package manager we're using
+# and install packages from dot_packages
+if [ -f /etc/debian_version ]; then
+    sudo apt-get install $(cat dot_packages)
+elif [ -f /etc/redhat-release ]; then
+    sudo yum install $(cat dot_packages)
+elif [ -f /etc/arch-release ]; then
+    sudo paru -S $(cat dot_packages) --needed
+elif [ -f /etc/gentoo-release ]; then
+    sudo emerge $(cat dot_packages)
+else
+    echo "Unknown distribution"
+    exit 1
+fi
