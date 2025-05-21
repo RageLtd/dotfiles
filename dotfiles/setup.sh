b9ef24ed85bdba3eaaee50b6e35ad3ac65d5ad66
RageLtd <rageltd@pm.me>
2025-05-21 15:22:48 -0700

    Move into a subdir so cloning isn't pain

--- dotfiles/setup.sh
+++ dotfiles/setup.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
+IS_HOST=false
 # if host is passed in as an argument, skip some tools
 if [ "$1" = "--host" ]; then
     IS_HOST=true
-else
-    IS_HOST=false
 fi
-
 
 # Function to check if a command exists
 command_exists() {
@@ -27,7 +25,7 @@ install_packages() {
         sudo dnf install -y $limited
 
         if ! command_exists op && IS_HOST; then
-        	curl -f https://zed.dev/install.sh | sh
+            curl -f https://zed.dev/install.sh | sh
             sudo rpm --import https://downloads.1password.com/linux/keys/1password.asc
             sudo sh -c 'echo -e "[1password]\nname=1Password Stable Channel\nbaseurl=https://downloads.1password.com/linux/rpm/stable/\$basearch\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=\"https://downloads.1password.com/linux/keys/1password.asc\"" > /etc/yum.repos.d/1password.repo'
             sudo dnf check-update -y 1password 1password-cli && sudo dnf install 1password 1password-cli
@@ -44,7 +42,7 @@ install_packages() {
 
     elif command_exists brew; then
         echo "Detected macOS. Using Homebrew."
-        brew install $packages zed 1password 1password-cli
+        brew install --adopt $packages zed 1password 1password-cli
     else
         echo "Error: No supported package manager found on this system."
         exit 1
@@ -61,13 +59,36 @@ fi
 # Install packages
 install_packages
 
-# Remove git artifacts
-rm -rf ~/.git
-rm .gitignore
+# Remove Readme and Licence
 rm README.md
 rm LICENCE
 
-if IS_HOST; then
-    op signin;
-    op inject -f -i .config/zed/settings.json.sample -o .config/zed/settings.json;
+source_dir="$HOME/dotfiles"
+target_dir="$HOME"
+
+find "$source_dir" -print0 | while IFS= read -r '' path; do
+    # Get the path relative to the source directory
+    relative_path=${path#$source_dir/}
+
+    # Construct the full destination path
+    destination_path="$target_dir/$relative_path"
+
+    if [ -d "$path" ]; then
+        # If it's a directory, create the corresponding directory in the target
+        mkdir -p "$destination_path"
+    elif [ -f "$path" ]; then
+        # If the file exists already, remove it
+        if [ -f "$destination_path" ]; then
+            rm "$destination_path"
+        fi
+        # Create a symlink to the original file
+        # Ensure the parent directory exists before creating the symlink
+        mkdir -p "$(dirname "$destination_path")"
+        ln -s "$(realpath "$path")" "$destination_path"
+    fi
+done
+
+if $IS_HOST; then
+    op signin
+    op inject -f -i .config/zed/settings.json.sample -o .config/zed/settings.json
 fi
